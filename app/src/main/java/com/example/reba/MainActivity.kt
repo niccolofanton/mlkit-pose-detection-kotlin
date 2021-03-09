a971c0c649437b5e5edf04e57c36553371970597
Niccolò <niccolofanton1997@gmail.com>
2021-03-09 18:16:57 +0100

    first commit

---
+++ app/src/main/java/com/example/reba/MainActivity.kt
@@ -0,0 +1,441 @@
+package com.example.reba
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.*
+import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewConfiguration
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.pose.Pose
+import com.google.mlkit.vision.pose.PoseDetection
+import com.google.mlkit.vision.pose.PoseLandmark
+import com.google.mlkit.vision.pose.accurate.AccuratePoseDetectorOptions
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import kotlin.math.atan2
+
+
+private class PoseAnalyzer(private val poseFoundListener: (Pose) -> Unit) : ImageAnalysis.Analyzer {
+
+    private val options = AccuratePoseDetectorOptions.Builder()
+    .setDetectorMode(AccuratePoseDetectorOptions.STREAM_MODE)
+    .build()
+
+    private val poseDetector = PoseDetection.getClient(options);
+
+    @SuppressLint("UnsafeExperimentalUsageError")
+    override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+            // Accurate pose detector on static images, when depending on the pose-detection-accurate sdk
+
+            poseDetector
+                    .process(image)
+                    .addOnSuccessListener { pose ->
+                        poseFoundListener(pose)
+                        imageProxy.close()
+                    }
+                    .addOnFailureListener { error ->
+                        Log.d(TAG, "Failed to process the image")
+                        error.printStackTrace()
+                        imageProxy.close()
+                    }
+        }
+    }
+}
+
+class RectOverlay constructor(context: Context?, attributeSet: AttributeSet?) :
+    View(context, attributeSet) {
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+    private val STROKE_WIDTH = 3f // has to be float
+    private val drawColor = Color.RED
+    // Set up the paint with which to draw.
+    private val paint = Paint().apply {
+        color = drawColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device are down-sampled.
+        isDither = true
+        style = Paint.Style.STROKE // default: FILL
+        strokeJoin = Paint.Join.ROUND // default: MITER
+        strokeCap = Paint.Cap.ROUND // default: BUTT
+        strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+    }
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(Color.TRANSPARENT)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
+
+     fun clear() {
+        extraCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+    }
+
+    internal fun drawLine(
+        startLandmark: PoseLandmark?,
+        endLandmark: PoseLandmark?
+    ) {
+        val start = startLandmark!!.position
+        val end = endLandmark!!.position
+
+
+        val xmul = 3.3f;
+        val ymul = 3.3f;
+
+        extraCanvas.drawLine(
+            (start.x * xmul) - 250, start.y* ymul, (end.x* xmul) -250, end.y* ymul, paint
+        )
+        invalidate();
+    }
+
+    internal fun drawLine2(
+        x1: Float,
+        y1: Float,
+        x2: Float,
+        y2: Float
+    ) {
+
+         val paint2 = Paint().apply {
+            color = Color.GREEN
+            // Smooths out edges of what is drawn without affecting shape.
+            isAntiAlias = true
+            // Dithering affects how colors with higher-precision than the device are down-sampled.
+            isDither = true
+            style = Paint.Style.STROKE // default: FILL
+            strokeJoin = Paint.Join.ROUND // default: MITER
+            strokeCap = Paint.Cap.ROUND // default: BUTT
+            strokeWidth = 10f // default: Hairline-width (really thin)
+        }
+
+        val mul = 1f;
+        extraCanvas.drawLine(
+            x1 * mul, y1* mul, x2* mul, y2* mul, paint2
+        )
+        invalidate();
+    }
+
+
+}
+
+class MainActivity : AppCompatActivity() {
+    private var imageCapture: ImageCapture? = null
+
+    private lateinit var cameraExecutor: ExecutorService
+
+    private lateinit var textView : TextView;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                    this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        // Set up the listener for take photo button
+//        camera_capture_button.setOnClickListener { takePhoto() }
+
+        textView = findViewById(R.id.text_view_id)
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int, permissions: Array<String>, grantResults:
+            IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                        "Permissions not granted by the user.",
+                        Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    fun getAngle(firstPoint: PoseLandmark, midPoint: PoseLandmark, lastPoint: PoseLandmark): Double {
+
+        var result = Math.toDegrees(atan2( lastPoint.getPosition().y.toDouble() - midPoint.getPosition().y,
+            lastPoint.getPosition().x.toDouble() - midPoint.getPosition().x)
+                - atan2(firstPoint.getPosition().y - midPoint.getPosition().y,
+            firstPoint.getPosition().x - midPoint.getPosition().x))
+        result = Math.abs(result) // Angle should never be negative
+        if (result > 180) {
+            result = 360.0 - result // Always get the acute representation of the angle
+        }
+        return result
+    }
+
+    private fun onTextFound(pose: Pose)  {
+        try {
+
+
+            val leftShoulder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER)
+            val rightShoulder = pose.getPoseLandmark(PoseLandmark.RIGHT_SHOULDER)
+            val leftElbow = pose.getPoseLandmark(PoseLandmark.LEFT_ELBOW)
+            val rightElbow = pose.getPoseLandmark(PoseLandmark.RIGHT_ELBOW)
+            val leftWrist = pose.getPoseLandmark(PoseLandmark.LEFT_WRIST)
+            val rightWrist = pose.getPoseLandmark(PoseLandmark.RIGHT_WRIST)
+            val leftHip = pose.getPoseLandmark(PoseLandmark.LEFT_HIP)
+            val rightHip = pose.getPoseLandmark(PoseLandmark.RIGHT_HIP)
+            val leftKnee = pose.getPoseLandmark(PoseLandmark.LEFT_KNEE)
+            val rightKnee = pose.getPoseLandmark(PoseLandmark.RIGHT_KNEE)
+            val leftAnkle = pose.getPoseLandmark(PoseLandmark.LEFT_ANKLE)
+            val rightAnkle = pose.getPoseLandmark(PoseLandmark.RIGHT_ANKLE)
+
+            val leftPinky = pose.getPoseLandmark(PoseLandmark.LEFT_PINKY)
+            val rightPinky = pose.getPoseLandmark(PoseLandmark.RIGHT_PINKY)
+            val leftIndex = pose.getPoseLandmark(PoseLandmark.LEFT_INDEX)
+            val rightIndex = pose.getPoseLandmark(PoseLandmark.RIGHT_INDEX)
+            val leftThumb = pose.getPoseLandmark(PoseLandmark.LEFT_THUMB)
+            val rightThumb = pose.getPoseLandmark(PoseLandmark.RIGHT_THUMB)
+            val leftHeel = pose.getPoseLandmark(PoseLandmark.LEFT_HEEL)
+            val rightHeel = pose.getPoseLandmark(PoseLandmark.RIGHT_HEEL)
+            val leftFootIndex = pose.getPoseLandmark(PoseLandmark.LEFT_FOOT_INDEX)
+            val rightFootIndex = pose.getPoseLandmark(PoseLandmark.RIGHT_FOOT_INDEX)
+
+
+            val builder = StringBuilder()
+
+
+            val occhioSx = pose.getPoseLandmark(PoseLandmark.LEFT_EYE_INNER);
+            val occhioDx = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE_INNER);
+
+//            val fineColloX = occhioSx.getPosition().x + ((occhioDx.getPosition().x - occhioSx.getPosition().x) / 2);
+//            val fineColloY = occhioSx.getPosition().y + ((occhioDx.getPosition().y - occhioSx.getPosition().y) / 2);
+//            val inizioColloX = leftShoulder.getPosition().x + ((rightShoulder.getPosition().x - leftShoulder.getPosition().x) / 2);
+//            val inizioColloY =leftShoulder.getPosition().y + ((rightShoulder.getPosition().y - leftShoulder.getPosition().y) / 2);
+//            rect_overlay.drawLine2(leftShoulder.getPosition().x, leftShoulder.getPosition().y, rightShoulder.getPosition().x, rightShoulder.getPosition().y)
+//            builder.append("$inizioColloX, $inizioColloY, $fineColloX, $fineColloY \n")
+
+            rect_overlay.drawLine(occhioSx, occhioDx)
+
+
+            builder.append("${occhioSx!!.position.x} ${occhioSx!!.position.y} \n")
+
+
+            if(rightShoulder != null && rightElbow != null && rightWrist != null){
+                val angoloBraccio = getAngle(
+                    rightShoulder,
+                    rightElbow,
+                    rightWrist)
+                    builder.append("${angoloBraccio.toInt()} angolo braccio DX\n")
+            }
+
+
+            if(rightElbow != null && rightWrist != null && rightPinky != null){
+                val angoloPolso = getAngle(
+                    rightElbow,
+                    rightWrist,
+                    rightPinky
+                )
+                builder.append("${angoloPolso.toInt()} polso DX\n")
+            }
+
+            textView.setText("${builder.toString()}")
+
+
+            rect_overlay.clear()
+            if(leftShoulder != null && rightShoulder != null){
+                rect_overlay.drawLine(leftShoulder, rightShoulder)
+            }
+
+            if(leftHip != null &&  rightHip != null){
+                rect_overlay.drawLine(leftHip, rightHip)
+            }
+
+            if(leftShoulder != null &&  leftElbow != null){
+                rect_overlay.drawLine(leftShoulder, leftElbow)
+            }
+
+            if(leftElbow != null &&  leftWrist != null){
+                rect_overlay.drawLine(leftElbow, leftWrist)
+            }
+
+            if(leftShoulder != null &&  leftHip != null){
+                rect_overlay.drawLine(leftShoulder, leftHip)
+            }
+
+            if(leftHip != null &&  leftKnee != null){
+                rect_overlay.drawLine(leftHip, leftKnee)
+            }
+
+            if(leftKnee != null &&  leftAnkle != null){
+                rect_overlay.drawLine(leftKnee, leftAnkle)
+            }
+
+            if(leftWrist != null &&  leftThumb != null){
+                rect_overlay.drawLine(leftWrist, leftThumb)
+            }
+
+            if(leftWrist != null &&  leftPinky != null){
+                rect_overlay.drawLine(leftWrist, leftPinky)
+            }
+
+            if(leftWrist != null &&  leftIndex != null){
+                rect_overlay.drawLine(leftWrist, leftIndex)
+            }
+
+            if(leftIndex != null &&  leftPinky != null){
+                rect_overlay.drawLine(leftIndex, leftPinky)
+            }
+
+            if(leftAnkle != null &&  leftHeel != null){
+                rect_overlay.drawLine(leftAnkle, leftHeel)
+            }
+
+            if(leftHeel != null &&  leftFootIndex != null){
+                rect_overlay.drawLine(leftHeel, leftFootIndex)
+            }
+
+            if(rightShoulder != null &&  rightElbow != null){
+                rect_overlay.drawLine(rightShoulder, rightElbow)
+            }
+
+            if(rightElbow != null &&  rightWrist != null){
+                rect_overlay.drawLine(rightElbow, rightWrist)
+            }
+
+            if(rightShoulder != null &&  rightHip != null){
+                rect_overlay.drawLine(rightShoulder, rightHip)
+            }
+
+            if(rightHip != null &&  rightKnee != null){
+                rect_overlay.drawLine(rightHip, rightKnee)
+            }
+
+            if(rightKnee != null &&  rightAnkle != null){
+                rect_overlay.drawLine(rightKnee, rightAnkle)
+            }
+
+            if(rightWrist != null &&  rightThumb != null){
+                rect_overlay.drawLine(rightWrist, rightThumb)
+            }
+
+            if(rightWrist != null &&  rightPinky != null){
+                rect_overlay.drawLine(rightWrist, rightPinky)
+            }
+
+            if(rightWrist != null &&  rightIndex != null){
+                rect_overlay.drawLine(rightWrist, rightIndex)
+            }
+
+            if(rightIndex != null &&  rightPinky != null){
+                rect_overlay.drawLine(rightIndex, rightPinky)
+            }
+
+            if(rightAnkle != null &&  rightHeel != null){
+                rect_overlay.drawLine(rightAnkle, rightHeel)
+            }
+
+            if(rightHeel != null &&  rightFootIndex != null){
+                rect_overlay.drawLine(rightHeel, rightFootIndex)
+            }
+
+        } catch (e: java.lang.Exception) {
+            Toast.makeText(this@MainActivity, "Errore", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                    .build()
+                    .also {
+                        it.setSurfaceProvider(viewFinder.createSurfaceProvider())
+                    }
+
+
+            val imageAnalyzer = ImageAnalysis.Builder()
+                    .build()
+                    .also {
+                        it.setAnalyzer(cameraExecutor,PoseAnalyzer(::onTextFound))
+                    }
+
+            imageCapture = ImageCapture.Builder()
+                    .build()
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                        this, cameraSelector, preview, imageCapture, imageAnalyzer)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+                baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+}
